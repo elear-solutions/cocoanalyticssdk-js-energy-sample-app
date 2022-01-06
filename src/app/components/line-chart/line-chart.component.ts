62ea9500f134ed36a1880573fe623018e31ff99c
soujanya.venkatesh@gmail.com <soujanya.venkatesh@gmail.com>
2022-01-06 19:06:08 +0530

    Pending Items

--- src/app/components/line-chart/line-chart.component.ts
+++ src/app/components/line-chart/line-chart.component.ts
@@ -122,46 +122,46 @@ export class LineChartComponent implements OnChanges {
   ngOnChanges(): void {
     this.barChartLabels = [];
     this.barChartColors = [];
-    // this.barChartData = [
-    //   {
-    //     data: [], label: '', fill: false, backgroundColor: [
-    //       'rgba(255, 99, 132, 0.2)',
-    //       'rgba(54, 162, 235, 0.2)',
-    //       'rgba(255, 206, 86, 0.2)',
-    //       'rgba(75, 192, 192, 0.2)',
-    //       'rgba(153, 102, 255, 0.2)',
-    //       'rgba(255, 159, 64, 0.2)'
-    //     ],
-    //     borderColor: [
-    //       'rgba(255, 99, 132, 1)',
-    //       'rgba(54, 162, 235, 1)',
-    //       'rgba(255, 206, 86, 1)',
-    //       'rgba(75, 192, 192, 1)',
-    //       'rgba(153, 102, 255, 1)',
-    //       'rgba(255, 159, 64, 1)'
-    //     ],
-    //     borderWidth: 1
-    //   },
+    this.barChartData = [
+      {
+        data: [], label: '', fill: false, backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
 
-    //   {
-    //     data: [], label: '', fill: false, backgroundColor: [
-    //       'rgba(255, 99, 132, 0.2)',
-    //       'rgba(54, 162, 235, 0.2)',
-    //       'rgba(255, 206, 86, 0.2)',
-    //       'rgba(75, 192, 192, 0.2)',
-    //       'rgba(153, 102, 255, 0.2)',
-    //       'rgba(255, 159, 64, 0.2)'
-    //     ],
-    //     borderColor: [
-    //       'rgba(255, 99, 132, 1)',
-    //       'rgba(54, 162, 235, 1)',
-    //       'rgba(255, 206, 86, 1)',
-    //       'rgba(75, 192, 192, 1)',
-    //       'rgba(153, 102, 255, 1)',
-    //       'rgba(255, 159, 64, 1)'
-    //     ],
-    //     borderWidth: 1
-    //   }];
+      {
+        data: [], label: '', fill: false, backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }];
     var k = 1;
 
     if (this.dataset) {
@@ -169,7 +169,6 @@ export class LineChartComponent implements OnChanges {
         this.barChartLabels.push(k.toString());
         k++;
       }
-      alert("labels");
 
       for (var i = 0; i < this.dataset.length; i++) {
         for (var j = 0; j < this.dataset[i].data?.length; j++) {
